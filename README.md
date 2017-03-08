97196546d21f96052306bc2ecd57f0c1d29d4bac
Steven Saus <steven.saus@gmail.com>
2017-03-08 15:53:26 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # saurian-web-history-pollution
 A script to pollute your web history a little bit to help obfuscate ISP snooping
+
+In 2016, the FCC ruled that internet service providers had to [get your permission before selling your raw browsing data
+](http://www.recode.net/2016/10/28/13442880/internet-providers-fcc-permission-share-web-browsing-data-opt-in).
+
+While that wasn't hard for them to do, the [Trump-led GOP is trying to remove that tiny bit of privacy](https://arstechnica.com/tech-policy/2017/03/gop-senators-new-bill-would-let-isps-sell-your-web-browsing-data/).
+
+While there's little substitute for tools such as HTTPS Everywhere, a VPN, and setting your DNS to ones other than your ISPs, this script is something I whipped up to pollute your web browsing history.
+
+It maintains a list of URLs - creating one at $HOME/.config/saurianspider.conf if needed - and retrieves them randomly at random-ish intervals. Any new links it finds on those pages, it'll add to the list. It also switches the useragent between Firefox, Chrome, Opera, Opera Mini, Edge, and Internet Exploder semi-randomly as well, thus making it more difficult to filter out these requests from your legit ones.
+
+The URL list is seeded with the current events page at Wikipedia and the "Random" page on Wikipedia; that said, it doesn't ADD links from Wikipedia or Wikimedia, as that could get really obvious, really quickly.
+
+If you want to use your own list of URLs in a different location, the file location should be the first (and only) argument.
+
+Depends upon/uses (most of these are GNU coreutils):
+
+curl
+awk
+sed
+head & tail 
+grep 
+mktemp 
+wc 
+shuf 
+
+Written in BASH, but may work okay with other shell variants. I haven't tested it.
---
+++ saurian_spider.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+
+# Requires 
+# awk
+# sed
+# head / tail
+# grep
+# curl
+
+########################################################################
+# Declarations
+########################################################################
+
+declare SpiderUrl
+declare ListOfUrls
+declare UrlToSpider
+declare UAString
+TempDir=$(mktemp -d)
+UserAgents=$TempDir/UAStrings
+
+
+##############################################################################
+# Writing UA String Tempfile
+###############################################################################
+write_uafile(){
+	# Add other strings here as you like. 
+	echo "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36" > $UserAgents
+	echo "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko" >> $UserAgents 
+	echo "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1" >> $UserAgents
+	echo "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246" >> $UserAgents
+	echo "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A" >> $UserAgents
+	echo "Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16" >> $UserAgents
+	echo "Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14" >> $UserAgents
+	echo "Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30" >> $UserAgents
+	echo "Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5; Trident/5.0; IEMobile/9.0)" >> $UserAgents
+	echo "Opera/9.80 (J2ME/MIDP; Opera Mini/9.80 (J2ME/23.377; U; en) Presto/2.5.25 Version/10.54" >> $UserAgents
+}
+
+##############################################################################
+# Getting the UA String
+###############################################################################
+get_uastring(){
+
+	ualines=1
+	uareadthisline=1
+	
+	ualines=$(wc -l < "$UserAgents")
+	uareadthisline=$(shuf -i 1-$ualines -n 1)
+	UAString=$(head -n $uareadthisline "$UserAgents" | tail -1)
+	echo "Using $UAString"
+	
+}
+
+##############################################################################
+# Getting the URL to spider and making sure it doesn't redirect
+###############################################################################
+get_url_to_spider() {
+	lines=1
+	readthisline=1
+	declare url
+	
+	lines=$(wc -l < "$ListOfUrls")
+	readthisline=$(shuf -i 1-$lines -n 1)
+	url=$(head -n $readthisline "$ListOfUrls" | tail -1)
+	get_uastring
+	UrlToSpider=$(curl -H "$UAString" $url -s -L -I -o /dev/null -w '%{url_effective}')
+	echo "We're going to crawl $UrlToSpider"
+	
+}
+
+##############################################################################
+# Spidering the URL and catching all the new links from it
+###############################################################################
+spider_url() {
+	
+	baseurl=$(echo "$UrlToSpider" | awk -F/ '{print $3}')
+	# echo "$baseurl"
+	# First grep cuts out links
+	# First cut trims it up
+	# Second grep cuts out filetypes we don't want
+	# First sed expression catches return urls without http:// prefix. 
+	# Second sed expression catches relative urls, prefixes base url
+	# Third and fourth grep expression catches wierd skype, mailto, etc links
+	# Last two cut out all the extra wikipedia/wikimedia links we don't need
+	# To force it to look outside wikipedia
+	curl -H "$UAString" "$UrlToSpider" 2>&1 | grep -o -E 'href="([^"#]+)"' | cut -d'"' -f2 | grep -v '\.\(css\|js\|png\|gif\|jpg\|ico\|jpeg\|pdf\)$' | sed '/^\/\//s#//#http://#' | sed "/^\//s#/#$baseurl/#" | grep '^http' | grep -v 'action=' | grep -v 'wikipedia' | grep -v 'wikimedia' > $TempDir/RawUrls
+
+}
+
+
+##############################################################################
+# Removing duplicates with our little saurian arms
+###############################################################################
+saurian_url() {
+
+	numurls=$(wc -l < "$TempDir/RawUrls")
+	echo "There were $numurls gotten from that link..."
+	sort $TempDir/RawUrls | uniq > $TempDir/BigUrls.merged
+	sort $ListOfUrls | uniq >> $TempDir/BigUrls.merged
+	sort $TempDir/BigUrls.merged | uniq > $TempDir/BigUrls.sorted
+	cat $TempDir/BigUrls.sorted > $ListOfUrls
+	# cleaning up after ourselves
+#	rm $TempDir/RawUrls
+#	rm $TempDir/BigUrls.sorted
+#	rm $TempDir/BigUrls.merged
+
+}
+
+###############################################################################
+# The actual loop of the program
+###############################################################################
+
+main() {
+
+
+	increment=0
+		
+	while :
+		do
+			get_url_to_spider
+			spider_url
+			saurian_url
+			numurls=$(wc -l < "$ListOfUrls")
+			(( ++increment ))
+			interval=$(shuf -i 1-30 -n 1)
+			echo "$increment URLs crawled."
+			echo "$numurls URLs in list."
+			echo "Waiting $interval seconds."
+			echo "Press [CTRL+C] to stop.."			
+			sleep $interval
+		done
+}
+
+
+###############################################################################
+# Startup Sanity Checker
+###############################################################################
+
+echo "Welcome to Saurian Spider, your friendly web history obfuscator!"
+
+
+if [ ! -f "$1" ];then
+	if [ ! -f "$HOME/.config/saurianspider.conf" ];then
+		echo "Initializing list of URLs at $HOME/.config/saurianspider.conf"
+		echo "https://en.wikipedia.org/wiki/Special:Randompage" > $HOME/.config/saurianspider.conf
+		echo "https://en.wikipedia.org/wiki/Portal:Current_events" >> $HOME/.config/saurianspider.conf
+	fi
+	echo "Using list of URLs at $HOME/.config/saurianspider.conf"	
+	ListOfUrls="$HOME/.config/saurianspider.conf"
+else
+	echo "Utilizing your list of URLs at $1"
+	ListOfUrls="$1"
+fi
+
+###############################################################################
+# Let's do it!
+###############################################################################
+
+write_uafile
+main
